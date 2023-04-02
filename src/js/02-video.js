56a111ca5665e8b3297da0a540ad1854e32a6250
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-04-03 00:19:10 +0300

    commit with coments

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,6 +20,40 @@ function saveCurrentTime() {
     }
   });
 
+
+   // // 2.0.1. 
+  // import Player from '@vimeo/player'; //  встановити пакет @vimeo/player $ npm install @vimeo/player
+  // import throttle from 'lodash.throttle'; // встановити пакет lodash.throttle $ npm install lodash.throttle
+  
+  // const iframe = document.querySelector('#vimeo-player'); // так ми отримуємо доступ до елемента по id
+  // iframe.classList.add('centered');// то я сам додав цей клас, щоб відео було по центру
+  // const player = new Player(iframe); // створюємо новий екземпляр класу Player це з документації
+  // player.on('timeupdate', throttle(saveCurrentTime, 1000)); // відслідковуємо подію timeupdate і викликаємо функцію saveCurrentTime
+  // // ця функція викликається не частіше ніж раз на 1000 мс
+  // // вона зберігає поточний час відтворення в localStorage
+  // // timeupdate - це подія, яка відбувається кожну секунду відтворення відео
+  // function saveCurrentTime() { // 
+  //     player.getCurrentTime().then(time => { //отже, як ми це робимо? ми викликаємо метод getCurrentTime() класу Player
+  //         // цей метод повертає проміс, тому ми використовуємо then
+  //         // в then ми отримуємо значення поточного часу відтворення відео
+  //         // і зберігаємо його в localStorage відповідно до ключа 'videoplayer-current-time'
+  //       localStorage.setItem('videoplayer-current-time', time); // сетимо значення в localStorage 
+  //       // videoplayer-current-time - це ключ, а time - це значення (videoplayer-current-time ми взяли з документації)
+  //     });
+  //   }
+  // // відслідковуємо подію 'load' і викликаємо функцію setCurrentTime
+  //   window.addEventListener('load', () => { 
+  //     const currentTime = localStorage.getItem('videoplayer-current-time'); // отримуємо значення з localStorage
+  //     if (currentTime) { // якщо значення є, то викликаємо метод setCurrentTime класу Player
+  //       player.setCurrentTime(currentTime); // тут ми встановлюємо поточний час відтворення
+  //       // тобто ми звертаємось до класу Player і викликаємо метод setCurrentTime якой передаємо значення currentTime
+  //       // яке ми отримали з localStorage. Метод setCurrentTime ми взяли з документації до Player
+  //       // відтворюємо відео з поточного часу
+  //       // player.play(); 
+  //     }
+  //   });
+
+
 // 2.1. 
 // const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
